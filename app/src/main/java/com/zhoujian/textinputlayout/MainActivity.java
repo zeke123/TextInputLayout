52635c9e56a24c85a7326b9da99a9ad09d081422
zhoujian <1064791247@qq.com>
2017-03-12 08:11:20 +0800

    TextInputLayout

---
+++ app/src/main/java/com/zhoujian/textinputlayout/MainActivity.java
@@ -0,0 +1,43 @@
+package com.zhoujian.textinputlayout;
+
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText mText_phone;
+    private TextInputEditText mText_password;
+    private Button mLogin;
+    private TextInputLayout mText_phone_layout;
+    private TextInputLayout mText_password_layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        initView();
+    }
+
+    private void initView()
+    {
+
+
+        mText_phone_layout = (TextInputLayout)findViewById(R.id.text_phone_layout);
+        mText_password_layout = (TextInputLayout)findViewById(R.id.text_password_layout);
+
+        mText_phone = (TextInputEditText)findViewById(R.id.text_phone);
+        mText_password = (TextInputEditText)findViewById(R.id.text_password);
+        mLogin = (Button)findViewById(R.id.login);
+
+
+
+
+
+
+    }
+}
